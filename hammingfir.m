4b940f2d32318fe3db6eb47ae34a399e1b0e9370
Hrishikesh Rao <hrishikeshrao6@gmail.com>
2025-11-26 01:24:26 +0530

    Update hammingfir.m

--- hammingfir.m
+++ hammingfir.m
@@ -18,3 +18,25 @@ stem(n,h);
 xlabel('n');
 ylabel('h(n');
 title('impulse response of the FIR filter');
+
+
+h=fir1(33, 150/(1000/2),hamming(34));
+
+%generate simulated input of 50, 300 & 200 Hz, each of 30 points
+n=1:30;
+f1=50;f2=300;f3=200;fs=1000;
+x=[];
+x1=sin(2*pi*n*f1/fs);
+x2=sin(2*pi*n*f2/fs);
+x3=sin(2*pi*n*f3/fs);
+x=[x1 x2 x3];
+subplot(2,1,1);
+stem(x);
+title('input');
+%generate o/p
+%y=conv(h,x);
+y=filter(h,1,x);
+subplot(2,1,2);
+stem(y);
+title('output');
+
